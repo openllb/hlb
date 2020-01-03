4dffb791fc4588eeca8a1600519aed8186789e90
Edgar Lee <edgarl@netflix.com>
2020-01-03 15:18:15 -0800

    Fix frontend build func

--- cmd/frontend/main.go
+++ cmd/frontend/main.go
@@ -5,11 +5,11 @@ import (
 
 	"github.com/moby/buildkit/frontend/gateway/grpcclient"
 	"github.com/moby/buildkit/util/appcontext"
-	"github.com/openllb/hlb"
+	"github.com/openllb/hlb/naive"
 )
 
 func main() {
-	if err := grpcclient.RunFromEnvironment(appcontext.Context(), hlb.Build); err != nil {
+	if err := grpcclient.RunFromEnvironment(appcontext.Context(), naive.Build); err != nil {
 		log.Printf("fatal error: %+v", err)
 		panic(err)
 	}
