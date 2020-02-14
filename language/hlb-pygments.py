1cad10b94af2ba17496a00e9d517beecf9e7efe4
Edgar Lee <edgarl@netflix.com>
2020-02-13 16:38:50 -0800

    Update pygment lexer

--- language/hlb-pygments.py
+++ language/hlb-pygments.py
@@ -15,34 +15,34 @@ class HlbLexer(RegexLexer):
         'root' : [
             (u'(#.*)', bygroups(Comment.Single)),
             (u'((\\b(0(b|B|o|O|x|X)[a-fA-F0-9]+)\\b)|(\\b(0|[1-9][0-9]*)\\b)|(\\b(true|false)\\b))', bygroups(Name.Constant)),
-            (u'(\")', bygroups(Punctuation), 'common__1'),
             (u'(\\bstring\\b|\\bint\\b|\\bbool\\b|\\bfs\\b|\\boption\\b)', bygroups(Keyword.Type)),
             (u'(\\b[a-zA-Z_][a-zA-Z0-9]*\\b)(\\()', bygroups(Keyword, Punctuation), 'params'),
-            (u'(\\))', bygroups(String)),
             (u'(\\{)', bygroups(Punctuation), 'block'),
-            (u'(\\})', bygroups(String)),
-            ('(\n|\r|\r\n)', String),
-            ('.', String),
-        ], 
+            ('(\n|\r|\r\n)', Text),
+            ('.', Text),
+        ],
+        'string': [
+            (r'"', String, '#pop'),
+            (r'\\"', String),
+            (r'[^\\"]+', String)
+        ],
         'block' : [
+            (u'(\\})', bygroups(Punctuation), '#pop'),
             (u'(#.*)', bygroups(Comment.Single)),
             (u'((\\b(0(b|B|o|O|x|X)[a-fA-F0-9]+)\\b)|(\\b(0|[1-9][0-9]*)\\b)|(\\b(true|false)\\b))', bygroups(Name.Constant)),
-            (u'(\")', bygroups(Punctuation), 'common__1'),
+            (r'"', String, 'string'),
             (u'(\\b(with|as|variadic)\\b)', bygroups(Name.Builtin)),
-            (u'(\\bstring\\b|\\bint\\b|\\bbool\\b|\\bfs\\b|\\boption\\b)([\\t ]+)(\\{)', bygroups(Keyword.Type, String, Punctuation), 'block'),
+            (u'(\\bstring\\b|\\bint\\b|\\bbool\\b|\\bfs\\b|\\boption\\b)([\\t ]+)(\\{)', bygroups(Keyword.Type, Text, Punctuation), 'block'),
             (u'(\\b((?!(scratch|image|resolve|http|checksum|chmod|filename|git|keepGitDir|local|includePatterns|excludePatterns|followPaths|generate|frontendInput|shell|run|readonlyRootfs|env|dir|user|network|security|host|ssh|secret|mount|target|localPath|uid|gid|mode|readonly|tmpfs|sourcePath|cache|mkdir|createParents|chown|createdTime|mkfile|rm|allowNotFound|allowWildcards|copy|followSymlinks|contentsOnly|unpack|createDestPath)\\b)[a-zA-Z_][a-zA-Z0-9]*\\b))', bygroups(Name.Variable)),
-            ('(\n|\r|\r\n)', String),
-            ('.', String),
-        ], 
-        'common__1' : [
-            ('(\n|\r|\r\n)', String),
-            ('.', String),
+            ('(\n|\r|\r\n)', Text),
+            ('.', Text),
         ], 
         'params' : [
+            (u'(\\))', bygroups(Punctuation), '#pop'),
             (u'(\\bstring\\b|\\bint\\b|\\bbool\\b|\\bfs\\b|\\boption\\b)', bygroups(Keyword.Type)),
             (u'(\\b[a-zA-Z_][a-zA-Z0-9]*\\b)', bygroups(Name.Variable)),
-            ('(\n|\r|\r\n)', String),
-            ('.', String),
+            ('(\n|\r|\r\n)', Text),
+            ('.', Text),
         ]
     }
 
