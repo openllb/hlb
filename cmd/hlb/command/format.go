448f248ee1a2af2d965b56d9448e4a4bf23cf8e3
Cory Bennett <cbennett@netflix.com>
2020-03-04 14:16:28 -0800

    refactor commands to not be tightly coupled to cobra

--- cmd/hlb/command/format.go
+++ cmd/hlb/command/format.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 
@@ -12,9 +13,9 @@ import (
 )
 
 var formatCommand = &cli.Command{
-	Name:    "format",
-	Aliases: []string{"fmt"},
-	Usage:   "formats HLB programs",
+	Name:      "format",
+	Aliases:   []string{"fmt"},
+	Usage:     "formats HLB programs",
 	ArgsUsage: "[ <*.hlb> ... ]",
 	Flags: []cli.Flag{
 		&cli.BoolFlag{
@@ -29,36 +30,48 @@ var formatCommand = &cli.Command{
 			return err
 		}
 		defer cleanup()
-
-		files, _, err := hlb.ParseMultiple(rs, defaultOpts()...)
-		if err != nil {
-			return err
-		}
-
-		_, err = report.SemanticCheck(files...)
-		if err != nil {
-			return err
-		}
-
-		if c.Bool("write") && c.NArg() > 0 {
-			for i, f := range files {
-				filename := lexer.NameOfReader(rs[i])
-				info, err := os.Stat(filename)
-				if err != nil {
-					return err
-				}
-
-				err = ioutil.WriteFile(filename, []byte(f.String()), info.Mode())
-				if err != nil {
-					return err
-				}
-			}
-		} else {
-			for _, f := range files {
-				fmt.Printf("%s", f)
-			}
-		}
-
-		return nil
+		return Format(rs, FormatOptions{
+			Write: c.Bool("write"),
+		})
 	},
+}
+
+type FormatOptions struct {
+	Write bool
+}
+
+func Format(rs []io.Reader, opts FormatOptions) error {
+	files, _, err := hlb.ParseMultiple(rs, defaultOpts()...)
+	if err != nil {
+		return err
+	}
+
+	_, err = report.SemanticCheck(files...)
+	if err != nil {
+		return err
+	}
+
+	if opts.Write {
+		for i, f := range files {
+			filename := lexer.NameOfReader(rs[i])
+			if filename == "" {
+				return fmt.Errorf("Unable to write, file name unavailable")
+			}
+			info, err := os.Stat(filename)
+			if err != nil {
+				return err
+			}
+
+			err = ioutil.WriteFile(filename, []byte(f.String()), info.Mode())
+			if err != nil {
+				return err
+			}
+		}
+	} else {
+		for _, f := range files {
+			fmt.Printf("%s", f)
+		}
+	}
+
+	return nil
 }
--- cmd/hlb/command/publish.go
+++ cmd/hlb/command/publish.go
@@ -3,8 +3,10 @@ package command
 import (
 	"context"
 	"fmt"
+	"io"
 	"strings"
 
+	"github.com/moby/buildkit/client"
 	"github.com/openllb/hlb"
 	"github.com/openllb/hlb/ast"
 	"github.com/openllb/hlb/codegen"
@@ -30,136 +32,146 @@ var publishCommand = &cli.Command{
 		},
 	},
 	Action: func(c *cli.Context) error {
+		ctx := context.Background()
+		cln, err := solver.BuildkitClient(ctx, c.String("addr"))
+		if err != nil {
+			return err
+		}
 		if !c.IsSet("ref") {
 			return fmt.Errorf("--ref must be specified")
 		}
-
 		rs, cleanup, err := collectReaders(c)
 		if err != nil {
 			return err
 		}
 		defer cleanup()
 
-		files, _, err := hlb.ParseMultiple(rs, defaultOpts()...)
-		if err != nil {
-			return err
-		}
-
-		sourceRoot, err := report.SemanticCheck(files...)
-		if err != nil {
-			return err
-		}
-
-		var params []*ast.Field
-		ast.Inspect(sourceRoot, func(node ast.Node) bool {
-			switch n := node.(type) {
-			case *ast.FuncDecl:
-				if n.Name.Name == c.String("target") {
-					params = n.Params.List
-					return false
-				}
-			case *ast.AliasDecl:
-				if n.Ident.Name == c.String("target") {
-					params = n.Func.Params.List
-					return false
-				}
-			}
-			return true
-		})
-
-		frontendStmts := []*ast.Stmt{
-			ast.NewCallStmt("frontendOpt", []*ast.Expr{
-				ast.NewStringExpr("hlb-target"),
-				ast.NewStringExpr(c.String("target")),
-			}, nil, nil),
-		}
-		for _, param := range params {
-			fun := "frontendOpt"
-			if param.Type.Type() == ast.Filesystem {
-				fun = "frontendInput"
-			}
-			frontendStmts = append(frontendStmts, ast.NewCallStmt(fun, []*ast.Expr{
-				ast.NewStringExpr(param.Name.Name),
-				ast.NewIdentExpr(param.Name.Name),
-			}, nil, nil))
-		}
-
-		var sources []string
-		for _, f := range files {
-			sources = append(sources, f.String())
-		}
-
-		signatureHLB := &ast.File{
-			Decls: []*ast.Decl{
-				{
-					Func: &ast.FuncDecl{
-						Type: ast.NewType(ast.Filesystem),
-						Name: ast.NewIdent(c.String("target")),
-						Params: &ast.FieldList{
-							List: params,
-						},
-						Body: &ast.BlockStmt{
-							List: []*ast.Stmt{
-								ast.NewCallStmt("generate", []*ast.Expr{
-									ast.NewBlockLitExpr(ast.Filesystem,
-										ast.NewCallStmt("image", []*ast.Expr{
-											ast.NewStringExpr(c.String("ref")),
-										}, nil, nil),
-									),
-								}, ast.NewWithBlockLit(frontendStmts...), nil),
-							},
-						},
-					},
-				},
-			},
-		}
-
-		entryName := "publish_hlb"
-		publishHLB := &ast.File{
-			Decls: []*ast.Decl{
-				{
-					Func: &ast.FuncDecl{
-						Type:   ast.NewType(ast.Filesystem),
-						Name:   ast.NewIdent(entryName),
-						Params: &ast.FieldList{},
-						Body: &ast.BlockStmt{
-							List: []*ast.Stmt{
-								ast.NewCallStmt("image", []*ast.Expr{
-									ast.NewStringExpr("openllb/hlb"),
-								}, nil, nil),
-								ast.NewCallStmt("mkfile", []*ast.Expr{
-									ast.NewStringExpr(hlb.SourceHLB),
-									ast.NewNumericExpr(int64(hlb.HLBFileMode), 8),
-									ast.NewStringExpr(strings.Join(sources, "")),
-								}, nil, nil),
-								ast.NewCallStmt("mkfile", []*ast.Expr{
-									ast.NewStringExpr(hlb.SignatureHLB),
-									ast.NewNumericExpr(int64(hlb.HLBFileMode), 8),
-									ast.NewStringExpr(signatureHLB.String()),
-								}, nil, nil),
-							},
-						},
-					},
-				},
-			},
-		}
-
-		root, err := report.SemanticCheck(publishHLB)
-		if err != nil {
-			return err
-		}
-
-		st, _, err := codegen.Generate(ast.NewCallStmt(entryName, nil, nil, nil).Call, root)
-		if err != nil {
-			return err
-		}
-
-		ctx := context.Background()
-		cln, err := solver.BuildkitClient(ctx, c.String("addr"))
-		if err != nil {
-			return err
-		}
-
-		return solver.Solve(ctx, cln, st, solver.WithPushImage(c.String("ref")))
+		return Publish(ctx, cln, c.String("ref"), c.String("default"), rs)
 	},
+}
+
+func Publish(ctx context.Context, cln *client.Client, ref string, target string, rs []io.Reader) error {
+	if ref == "" {
+		return fmt.Errorf("ref must not be empty")
+	}
+
+	if target == "" {
+		target = "default"
+	}
+
+	files, _, err := hlb.ParseMultiple(rs, defaultOpts()...)
+	if err != nil {
+		return err
+	}
+
+	sourceRoot, err := report.SemanticCheck(files...)
+	if err != nil {
+		return err
+	}
+
+	var params []*ast.Field
+	ast.Inspect(sourceRoot, func(node ast.Node) bool {
+		switch n := node.(type) {
+		case *ast.FuncDecl:
+			if n.Name.Name == target {
+				params = n.Params.List
+				return false
+			}
+		case *ast.AliasDecl:
+			if n.Ident.Name == target {
+				params = n.Func.Params.List
+				return false
+			}
+		}
+		return true
+	})
+
+	frontendStmts := []*ast.Stmt{
+		ast.NewCallStmt("frontendOpt", []*ast.Expr{
+			ast.NewStringExpr("hlb-target"),
+			ast.NewStringExpr(target),
+		}, nil, nil),
+	}
+	for _, param := range params {
+		fun := "frontendOpt"
+		if param.Type.Type() == ast.Filesystem {
+			fun = "frontendInput"
+		}
+		frontendStmts = append(frontendStmts, ast.NewCallStmt(fun, []*ast.Expr{
+			ast.NewStringExpr(param.Name.Name),
+			ast.NewIdentExpr(param.Name.Name),
+		}, nil, nil))
+	}
+
+	var sources []string
+	for _, f := range files {
+		sources = append(sources, f.String())
+	}
+
+	signatureHLB := &ast.File{
+		Decls: []*ast.Decl{
+			{
+				Func: &ast.FuncDecl{
+					Type: ast.NewType(ast.Filesystem),
+					Name: ast.NewIdent(target),
+					Params: &ast.FieldList{
+						List: params,
+					},
+					Body: &ast.BlockStmt{
+						List: []*ast.Stmt{
+							ast.NewCallStmt("generate", []*ast.Expr{
+								ast.NewBlockLitExpr(ast.Filesystem,
+									ast.NewCallStmt("image", []*ast.Expr{
+										ast.NewStringExpr(ref),
+									}, nil, nil),
+								),
+							}, ast.NewWithBlockLit(frontendStmts...), nil),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	entryName := "publish_hlb"
+	publishHLB := &ast.File{
+		Decls: []*ast.Decl{
+			{
+				Func: &ast.FuncDecl{
+					Type:   ast.NewType(ast.Filesystem),
+					Name:   ast.NewIdent(entryName),
+					Params: &ast.FieldList{},
+					Body: &ast.BlockStmt{
+						List: []*ast.Stmt{
+							ast.NewCallStmt("image", []*ast.Expr{
+								ast.NewStringExpr("openllb/hlb"),
+							}, nil, nil),
+							ast.NewCallStmt("mkfile", []*ast.Expr{
+								ast.NewStringExpr(hlb.SourceHLB),
+								ast.NewNumericExpr(int64(hlb.HLBFileMode), 8),
+								ast.NewStringExpr(strings.Join(sources, "")),
+							}, nil, nil),
+							ast.NewCallStmt("mkfile", []*ast.Expr{
+								ast.NewStringExpr(hlb.SignatureHLB),
+								ast.NewNumericExpr(int64(hlb.HLBFileMode), 8),
+								ast.NewStringExpr(signatureHLB.String()),
+							}, nil, nil),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	root, err := report.SemanticCheck(publishHLB)
+	if err != nil {
+		return err
+	}
+
+	st, _, err := codegen.Generate(ast.NewCallStmt(entryName, nil, nil, nil).Call, root)
+	if err != nil {
+		return err
+	}
+
+	return solver.Solve(ctx, cln, st, solver.WithPushImage(ref))
 }
