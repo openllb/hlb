622b88ecd601deb743c81995c34458cd7606d504
Josh Leder <jleder@netflix.com>
2020-08-20 10:21:01 -0600

    BufferedImageResolver is an ImageMetaResolver with a public Buffer

--- codegen/metadata.go
+++ codegen/metadata.go
@@ -3,40 +3,38 @@ package codegen
 import (
 	"context"
 
-	"github.com/moby/buildkit/client/llb"
-
 	"github.com/containerd/containerd/content"
-
-	"github.com/opencontainers/go-digest"
-
 	"github.com/containerd/containerd/images"
 	"github.com/containerd/containerd/platforms"
 	"github.com/containerd/containerd/reference"
 	"github.com/containerd/containerd/remotes"
-	"github.com/moby/buildkit/util/imageutil"
-
 	"github.com/containerd/containerd/remotes/docker"
+	"github.com/moby/buildkit/client/llb"
 	"github.com/moby/buildkit/util/contentutil"
+	"github.com/moby/buildkit/util/imageutil"
+	"github.com/opencontainers/go-digest"
 	specs "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
-type SharedImageResolver struct {
+// BufferedImageResolver is an image resolver with a public Buffer. It implements the
+// llb.ImageMetaResolver, content.Provider, and content.Ingester interfaces.
+type BufferedImageResolver struct {
 	contentutil.Buffer
 	resolver remotes.Resolver
 }
 
-type SharedResolverOpt func(*SharedImageResolver)
+type ResolverOpt func(*BufferedImageResolver)
 
-func WithContent(buffer contentutil.Buffer) SharedResolverOpt {
-	return func(ir *SharedImageResolver) {
-		ir.Buffer = buffer
+func WithBuffer(buffer contentutil.Buffer) ResolverOpt {
+	return func(bir *BufferedImageResolver) {
+		bir.Buffer = buffer
 	}
 }
 
-// NewSharedImageResolver returns an ImageMetaResolver that can share its content. It implements content.Provider
-// so that the images package can read manifests from fetched manifest lists.
-func NewSharedImageResolver(with ...SharedResolverOpt) *SharedImageResolver {
-	ir := &SharedImageResolver{
+// NewBufferedImageResolver returns a resolver that exposes its content so that the consumers can read manifests
+// and other descriptors from the fetched index.
+func NewBufferedImageResolver(with ...ResolverOpt) *BufferedImageResolver {
+	ir := &BufferedImageResolver{
 		Buffer:   contentutil.NewBuffer(),
 		resolver: docker.NewResolver(docker.ResolverOptions{}),
 	}
@@ -47,46 +45,22 @@ func NewSharedImageResolver(with ...SharedResolverOpt) *SharedImageResolver {
 }
 
 // ResolveImageConfig fetches descriptors from ref from the remote registry. It returns the manifest list digest and
-// the image config as raw JSON bytes. After returning successfully, the SharedImageResolver can be queried for
+// the image config as raw JSON bytes. After returning successfully, the BufferedImageResolver can be queried for
 // all other descriptors associated with the ref.
-func (sir *SharedImageResolver) ResolveImageConfig(ctx context.Context, ref string, opt llb.ResolveImageConfigOpt) (digest.Digest, []byte, error) {
-	r, err := reference.Parse(ref)
+func (bir *BufferedImageResolver) ResolveImageConfig(ctx context.Context, ref string, opt llb.ResolveImageConfigOpt) (digest.Digest, []byte, error) {
+	desc, err := bir.ResolveDescriptor(ctx, ref)
 	if err != nil {
 		return "", nil, err
 	}
 
-	desc := specs.Descriptor{
-		Digest: r.Digest(),
-	}
-
-	// try to fetch the given digest directly from the content store
-	if desc.Digest != "" {
-		ra, err := sir.ReaderAt(ctx, desc)
-		if err == nil {
-			desc.Size = ra.Size()
-			mt, err := imageutil.DetectManifestMediaType(ra)
-			if err == nil {
-				desc.MediaType = mt
-			}
-		}
-	}
-
-	// use resolver if desc is incomplete
-	if desc.MediaType == "" {
-		_, desc, err = sir.resolver.Resolve(ctx, r.String())
-		if err != nil {
-			return "", nil, err
-		}
-	}
-
-	fetcher, err := sir.resolver.Fetcher(ctx, r.String())
+	fetcher, err := bir.resolver.Fetcher(ctx, ref)
 	if err != nil {
 		return desc.Digest, nil, err
 	}
 
 	handlers := images.Handlers(
-		remotes.FetchHandler(sir, fetcher),
-		images.ChildrenHandler(sir),
+		remotes.FetchHandler(bir, fetcher),
+		images.ChildrenHandler(bir),
 	)
 
 	// recursively fetches OCI descriptors into buffer
@@ -102,15 +76,56 @@ func (sir *SharedImageResolver) ResolveImageConfig(ctx context.Context, ref stri
 		p = platforms.Only(*opt.Platform)
 	}
 
-	config, err := images.Config(ctx, sir, desc, p)
+	config, err := images.Config(ctx, bir, desc, p)
 	if err != nil {
 		return "", nil, err
 	}
 
-	dt, err := content.ReadBlob(ctx, sir, config)
+	dt, err := content.ReadBlob(ctx, bir, config)
 	if err != nil {
 		return "", nil, err
 	}
 
 	return desc.Digest, dt, nil
+}
+
+// ResolveDescriptor returns a specs.Descriptor by first trying to load by digest from the local store, or
+// else falling back to resolving ref against the remote registry.
+func (bir *BufferedImageResolver) ResolveDescriptor(ctx context.Context, ref string) (specs.Descriptor, error) {
+	r, err := reference.Parse(ref)
+	if err != nil {
+		return specs.Descriptor{}, err
+	}
+
+	desc, _ := bir.DigestDescriptor(ctx, r.Digest())
+
+	// use resolver if desc is incomplete
+	if desc.MediaType == "" {
+		var err error
+		_, desc, err = bir.resolver.Resolve(ctx, r.String())
+		if err != nil {
+			return desc, err
+		}
+	}
+
+	return desc, nil
+}
+
+// DigestDescriptor returns a specs.Descriptor for the given digest, or an error if the
+// content is not found. It does not attempt to fetch the digest remotely.
+func (bir *BufferedImageResolver) DigestDescriptor(ctx context.Context, dgst digest.Digest) (specs.Descriptor, error) {
+	desc := specs.Descriptor{Digest: dgst}
+	ra, err := bir.ReaderAt(ctx, desc)
+	if err != nil {
+		return desc, err
+	}
+	defer ra.Close()
+
+	desc.Size = ra.Size()
+	mt, err := imageutil.DetectManifestMediaType(ra)
+	if err != nil {
+		return desc, err
+	}
+	desc.MediaType = mt
+	return desc, nil
 }
