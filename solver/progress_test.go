2b6b28f9fdf8201ca3f5e182301c6a7d0f297cf9
Edgar Lee <edgarl@netflix.com>
2022-02-09 12:00:20 -0800

    Add progress tests and remove unused Write function

---
+++ solver/progress_test.go
@@ -0,0 +1,85 @@
+package solver
+
+import (
+	"context"
+	"io"
+	"io/ioutil"
+	"strings"
+	"testing"
+
+	"github.com/creack/pty"
+	"github.com/docker/buildx/util/progress"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProgress(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		name string
+		fn   func(p Progress) error
+	}
+
+	ctx := context.Background()
+	for _, tc := range []testCase{{
+		"empty opts", nil,
+	}, {
+		"output empty sync",
+		func(p Progress) error {
+			// Can sync from beginning.
+			err := p.Sync()
+			if err != nil {
+				return err
+			}
+
+			// Can sync after sync.
+			return p.Sync()
+		},
+	}, {
+		"output sync after write",
+		func(p Progress) error {
+			pw := p.MultiWriter().WithPrefix("", false)
+			progress.FromReader(pw, "test", io.NopCloser(strings.NewReader("")))
+
+			// Can sync after write.
+			return p.Sync()
+		},
+	}} {
+		for _, mode := range []string{"tty", "plain"} {
+			tc, mode := tc, mode
+			t.Run(tc.name+" "+mode, func(t *testing.T) {
+				ptm, pts, err := pty.Open()
+				require.NoError(t, err)
+
+				var opts []ProgressOption
+				switch mode {
+				case "tty":
+					opts = append(opts, WithLogOutput(pts, LogOutputTTY))
+				case "plain":
+					opts = append(opts, WithLogOutput(pts, LogOutputPlain))
+				}
+
+				p, err := NewProgress(ctx, opts...)
+				require.NoError(t, err)
+
+				if tc.fn != nil {
+					err = tc.fn(p)
+					require.NoError(t, err)
+				}
+
+				p.Release()
+				err = p.Wait()
+				require.NoError(t, err)
+
+				err = pts.Close()
+				require.NoError(t, err)
+
+				data, _ := ioutil.ReadAll(ptm)
+				t.Log("\n" + string(data))
+
+				err = ptm.Close()
+				require.NoError(t, err)
+			})
+		}
+	}
+}
