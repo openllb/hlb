882103dfc2352429ea41e69282ef2115957ef4be
Aaron Lehmann <alehmann@netflix.com>
2022-10-24 05:14:55 -0700

    Include last modified time in LocalUniqueID (#338)
    I ran into a situation where a "local" mount was wrongly deduped with a
    "local" from a concurrent solve that was created by another hlb process
    before some files in that dir were modified.
    
    By including the last modified timestamp in the unique ID, we an avoid
    this deduplication in the case where a file has actually changed.

--- pkg/llbutil/id.go
+++ pkg/llbutil/id.go
@@ -2,20 +2,26 @@ package llbutil
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/fs"
 	"net"
+	"os"
 	"strings"
+	"time"
 
 	"github.com/moby/buildkit/client/llb"
 	digest "github.com/opencontainers/go-digest"
+	"github.com/pkg/errors"
+	"github.com/tonistiigi/fsutil"
 )
 
 // LocalID returns a consistent hash for this local (path + options) so that
 // the same content doesn't get transported multiple times when referenced
 // repeatedly.
 func LocalID(ctx context.Context, absPath string, opts ...llb.LocalOption) (string, error) {
-	uniqID, err := LocalUniqueID(absPath)
+	uniqID, err := localUniqueID(absPath, opts...)
 	if err != nil {
 		return "", err
 	}
@@ -34,13 +40,60 @@ func LocalID(ctx context.Context, absPath string, opts ...llb.LocalOption) (stri
 	return digest.FromBytes(def.Def[len(def.Def)-1]).String(), nil
 }
 
-// LocalUniqueID returns a consistent string that is unique per host + cwd
-func LocalUniqueID(cwd string) (string, error) {
+// localUniqueID returns a consistent string that is unique per host + dir +
+// last modified time.
+//
+// If there is already a solve in progress using the same local dir, we want to
+// deduplicate the "local" if the directory hasn't changed, but if there has
+// been a change, we must not identify the "local" as a duplicate. Thus, we
+// incorporate the last modified timestamp into the result.
+func localUniqueID(localPath string, opts ...llb.LocalOption) (string, error) {
 	mac, err := FirstUpInterface()
 	if err != nil {
 		return "", err
 	}
-	return fmt.Sprintf("cwd:%s,mac:%s", cwd, mac), nil
+
+	fi, err := os.Stat(localPath)
+	if err != nil {
+		return "", err
+	}
+
+	lastModified := fi.ModTime()
+	if fi.IsDir() {
+		var localInfo llb.LocalInfo
+		for _, opt := range opts {
+			opt.SetLocalOption(&localInfo)
+		}
+
+		var walkOpts fsutil.WalkOpt
+		if localInfo.IncludePatterns != "" {
+			if err := json.Unmarshal([]byte(localInfo.IncludePatterns), &walkOpts.IncludePatterns); err != nil {
+				return "", errors.Wrap(err, "failed to unmarshal IncludePatterns for localUniqueID")
+			}
+		}
+		if localInfo.ExcludePatterns != "" {
+			if err := json.Unmarshal([]byte(localInfo.ExcludePatterns), &walkOpts.ExcludePatterns); err != nil {
+				return "", errors.Wrap(err, "failed to unmarshal ExcludePatterns for localUniqueID")
+			}
+		}
+		if localInfo.FollowPaths != "" {
+			if err := json.Unmarshal([]byte(localInfo.FollowPaths), &walkOpts.FollowPaths); err != nil {
+				return "", errors.Wrap(err, "failed to unmarshal FollowPaths for localUniqueID")
+			}
+		}
+
+		err := fsutil.Walk(context.Background(), localPath, &walkOpts, func(path string, info fs.FileInfo, err error) error {
+			if info.ModTime().After(lastModified) {
+				lastModified = info.ModTime()
+			}
+			return nil
+		})
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return fmt.Sprintf("path:%s,mac:%s,modified:%s", localPath, mac, lastModified.Format(time.RFC3339Nano)), nil
 }
 
 // FirstUpInterface returns the mac address for the first "UP" network
